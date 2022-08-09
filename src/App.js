cb8dad2dd0d18940d6c4d00edbfb1e9fc559f137
Pratheek G V <109038768+pratheek1904@users.noreply.github.com>
2022-08-09 21:47:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter,Route,Routes} from  'react-router-dom'
+import {BrowserRouter,Route,} from  'react-router-dom'
 import Home from './Home';
 import About from './About';
 import Project from './Project';
--- src/Home.js
+++ src/Home.js
@@ -11,14 +11,16 @@ const Home = () => {
        Hello I'am Pratheek G V, I have completed my Graduation in  Bangalore Institute Of Technology, I have started my front end in Refactor academy from June 01
        <br></br>
           <a href="https://github.com/pratheek1904"
-          target="_blank">Github Link</a>
+          target="_blank"  rel="noopener noreferrer">Github Link
+           </a>
           
           </section>
 
       </p>
 
       <div className="image">
-      <img src={image} height="400px" width="400px"/>
+    
+       <img src={image} alt="" height="400px" width="400px"/> 
       </div>
 
 
