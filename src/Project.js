ba4c9c3643e9cdfd7f10d9f4970c540ff9978ebc
Pratheek G V <109038768+pratheek1904@users.noreply.github.com>
2022-08-16 13:17:35 +0530

    Update Project.js

--- src/Project.js
+++ src/Project.js
@@ -6,7 +6,7 @@ const Project = () => {
       <h4><u>Projects List</u></h4>
       <ul>
       <li><a href="https://sparkling-naiad-e4cf09.netlify.app/">Todo List</a></li>
-      <li><a href="https://app.netlify.com/sites/velvety-kelpie-a210a3/deploys">Notes App Using Javascript</a></li>
+      <li><a href="https://aquamarine-faloodeh-5bd76b.netlify.app/">Notes App Using Javascript</a></li>
         <li>Simple Calculator</li>
         <li>Notes App Using React</li>
       
